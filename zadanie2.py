2a0ddcf883270c9bf85d15bc92a6f6dcfd83a8de
sad1boyzzz1z <67923643+sad1boyzzz1z@users.noreply.github.com>
2022-01-08 05:05:56 +0600

    Update zadanie2.py
    IDK

--- zadanie2.py
+++ zadanie2.py
@@ -1,16 +1,16 @@
-f = open('Perepis.txt', 'r')
+f = open('Perepis.txt')
 while True:
-	line = f.readln()
-	a = len(line)
-	if (line[a-2] <=7) and (line[a-1]<8):
-		b = line.split()
+	linejdd = f.readline()
+	a = len(linejdd)
+	if (linejdd[a-2] <=7) and (linejdd[a-1]<8):
+		b = linejdd.split()
 		print(b[0],b[1],b[2])
 
 diapazon1 = input()
 diapazon2 = input()
 while True:
-	lines = f.readln()
-	b = len(lines)
-	if (str(lines[b-2]+lines[b-1]) == diapazon1) or (str(lines[b-2]+lines[b-1]) == diapazon2):
-		g = lines.split()
+	liness = f.readline()
+	b = len(liness)
+	if (str(liness[b-2]+liness[b-1]) == diapazon1) or (str(liness[b-2]+liness[b-1]) == diapazon2):
+		g = liness.split()
 		print(b[0], b[1], b[2])
